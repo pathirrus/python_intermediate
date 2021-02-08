ddc0817f68dd89b31284b4d5e344701f764b2ef1
Patryq <p.kurc@o2.pl>
2021-02-08 20:43:17 +0100

    Added codes to files

---
+++ figures_exercises_oop/main.py
@@ -0,0 +1,29 @@
+from python_intermediate.figures_exercises_oop.figures_entities import Circle, Triangle, Rectangle, Figure, \
+    count_area_function
+
+
+def main():
+    circle = Circle(5)
+    circle2 = Circle(8)
+
+    triangle = Triangle(3, 7)
+    triangle2 = Triangle(12, 3)
+
+    rectangle = Rectangle(3, 3)
+    rectangle2 = Rectangle(4, 6)
+
+    print(circle.get_area(), triangle.get_area(), rectangle.get_area())
+
+    area = Figure.count_area([circle, triangle2, rectangle2, rectangle, circle2])
+    print(area)
+
+    area = count_area_function(circle, triangle, rectangle2)
+    print(area)
+
+    check_area = Figure.check_area(100, [circle,  triangle2])
+    print(check_area)
+    print(Figure.count_area([circle,  triangle2]))
+
+
+if __name__ == "__main__":
+    main()
---
+++ figures_exercises_oop/tests.py
@@ -0,0 +1,25 @@
+from python_intermediate.figures_exercises_oop.figures_entities import Circle, Triangle, Rectangle
+
+
+def test_check_area():
+    #  given
+    circle = Circle(1)
+    # circle2 = Circle(8)
+
+    triangle = Triangle(2, 1)
+    # triangle2 = Triangle(12, 3)
+
+    rectangle = Rectangle(2, 2)
+    # rectangle2 = Rectangle(4, 6)
+
+    #  when
+    result = Figure.count_area([circle, triangle, rectangle])
+
+    #  then
+    assert result == 8.142
+
+def test_compare_area():
+    #  given
+    circle = Circle(1)
+    triangle = Triangle(2, 1)
+    rectangle = Rectangle(2, 2)
