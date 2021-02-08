d5b4475e6bee2d8b823cf8fec788022378181c92
Patryq <p.kurc@o2.pl>
2021-02-08 20:07:05 +0100

    Edit cat file

---
+++ cat.py
@@ -0,0 +1,24 @@
+from sda_exercises_oop_1.animal import Animals
+from sda_exercises_oop_1.movable import Movable
+
+
+class Cat(Movable, Animals):
+
+    def __init__(self, name: str, sound="miau", eaten_mouse=0):
+        Animals.__init__(self, name)
+        self.sound = sound
+        self.eaten_mouse = eaten_mouse
+
+    def make_sound(self) -> str:
+        return f'Name is {self.name} sound: {self.sound}'
+
+    def eat_mouse(self) -> int:
+        self.eaten_mouse += 1
+        print(f'Zjadłem {self.eaten_mouse} myszy')
+        return self.eaten_mouse
+
+    def move(self) -> str:
+        return"idem"
+
+    def drink(self):
+        print(f'Cat drink water')
