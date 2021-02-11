f08f5ac218d449cfeb7c12137889adc4cb1180ff
Patryq <p.kurc@o2.pl>
2021-02-11 21:22:28 +0100

    Main file

---
+++ serializes/main.py
@@ -0,0 +1,10 @@
+from python_intermediate.serializes.pickle import pickle_write
+
+
+def main():
+    abbb = ['egg', 'TV', 'cola']
+    pickle_write(abbb)
+
+
+if __name__ == '__main__':
+    main()
