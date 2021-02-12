25fa9119258fd410e5a4ff7830630bfc1eed0693
Patryq <p.kurc@o2.pl>
2021-02-12 23:27:29 +0100

    Corrected import code for pytest

--- figures_exercises_oop/tests.py
+++ figures_exercises_oop/tests.py
@@ -1,6 +1,6 @@
 import pytest
 
-from python_intermediate.figures_exercises_oop.figures_entities import Circle, Triangle, Rectangle, Figure
+from .figures_entities import Circle, Triangle, Rectangle, Figure
 
 
 def test_check_area():
@@ -21,16 +21,16 @@ def test_check_area():
     assert result == 8.142
 
 
-# def test_compare_area():
-#     #  given
-#     circle = Circle(1)
-#     triangle = Triangle(2, 1)
-#     rectangle = Rectangle(2, 2)
-#
-#     some_area_big = Figure.count_area([circle, triangle, rectangle]) + 1
-#     some_area_small = Figure.count_area([circle, triangle, rectangle]) - 1
-#
-#     result_1 = Figure.count_area([circle, triangle, rectangle]) > some_area_small
-#     result_2 = Figure.count_area([circle, triangle, rectangle]) > some_area_big
-#
-#     assert result_1, result_2
+def test_compare_area():
+    #  given
+    circle = Circle(1)
+    triangle = Triangle(2, 1)
+    rectangle = Rectangle(2, 2)
+
+    some_area_big = Figure.count_area([circle, triangle, rectangle]) + 1
+    some_area_small = Figure.count_area([circle, triangle, rectangle]) - 1
+
+    result_1 = Figure.count_area([circle, triangle, rectangle]) > some_area_small
+    result_2 = Figure.count_area([circle, triangle, rectangle]) > some_area_big
+
+    assert result_1, result_2
