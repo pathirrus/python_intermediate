183f43e5fbafed8edc629227494534874e3054a3
Patryq <p.kurc@o2.pl>
2021-02-12 22:54:35 +0100

    Little changes

--- figures_exercises_oop/tests.py
+++ figures_exercises_oop/tests.py
@@ -1,4 +1,6 @@
-from python_intermediate.figures_exercises_oop.figures_entities import Circle, Triangle, Rectangle
+import pytest
+
+from python_intermediate.figures_exercises_oop.figures_entities import Circle, Triangle, Rectangle, Figure
 
 
 def test_check_area():
@@ -18,8 +20,17 @@ def test_check_area():
     #  then
     assert result == 8.142
 
-def test_compare_area():
-    #  given
-    circle = Circle(1)
-    triangle = Triangle(2, 1)
-    rectangle = Rectangle(2, 2)
+
+# def test_compare_area():
+#     #  given
+#     circle = Circle(1)
+#     triangle = Triangle(2, 1)
+#     rectangle = Rectangle(2, 2)
+#
+#     some_area_big = Figure.count_area([circle, triangle, rectangle]) + 1
+#     some_area_small = Figure.count_area([circle, triangle, rectangle]) - 1
+#
+#     result_1 = Figure.count_area([circle, triangle, rectangle]) > some_area_small
+#     result_2 = Figure.count_area([circle, triangle, rectangle]) > some_area_big
+#
+#     assert result_1, result_2
