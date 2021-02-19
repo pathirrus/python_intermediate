195c3616a494e42ddc8f4cd185ee92bb29c6a448
Patryq <p.kurc@o2.pl>
2021-02-19 21:34:23 +0100

    Static method for check birthday date

--- exercise_oop2_person/employee.py
+++ exercise_oop2_person/employee.py
@@ -1,4 +1,13 @@
-from  datetime import date
-from  python_intermediate_repo.python_intermediate.exercise_oop2_person.person import  Person
+from datetime import date
+from python_intermediate_repo.python_intermediate.exercise_oop2_person.person import Person
 
+
+class Employee(Person):
+
+    @staticmethod
+    def check_birth_date(value: date) -> date:
+        if value > date.today() or \
+                value < date(year=1920, month=12, day=31):
+            value = date(0, 0, 0)
+        return value
 
