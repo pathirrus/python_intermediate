629396d268ce5c7ba41f65d37243a896337b20f3
Patryq <p.kurc@o2.pl>
2021-02-19 21:22:39 +0100

    Add property and setter

--- exercise_oop2_person/person.py
+++ exercise_oop2_person/person.py
@@ -1,8 +1,32 @@
-import datetime
+from datetime import date
 
 
 class Person:
-    def __init__(self, name: str, surname: str, birth_date: datetime.date):
+    def __init__(self, name: str, surname: str, birth_date: date):
         self._name = name
         self._surname = surname
         self._birth_date = birth_date
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value: str):
+        self._name = value
+
+    @property
+    def surname(self):
+        return self._surname
+
+    @surname.setter
+    def surname(self, value: str):
+        self._surname = value
+
+    @property
+    def birth_date(self):
+        return self._birth_date
+
+    @birth_date.setter
+    def birth_date(self, value: date):
+        self._birth_date = value
