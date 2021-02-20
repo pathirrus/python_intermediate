227d6c2d16e69258b7d849398d4f9e85194a2ed1
Patryq <p.kurc@o2.pl>
2021-02-20 13:26:58 +0100

    example decoratores exercises

--- decorators/ex_ioerr.py
+++ decorators/ex_ioerr.py
@@ -1,3 +1,6 @@
+from six import wraps
+
+
 def catch_io_error_decorator(func):
     def inner(*args, **kwargs):
         try:
@@ -10,14 +13,31 @@ def catch_io_error_decorator(func):
     return inner
 
 
+def catch_io_error_with_wraps(func):
+
+    @wraps(func)
+    def inner(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+
+        except IOError as e:
+            print(f'IOerror cought: {e.args}')
+            return None
+
+    return inner
+
 @catch_io_error_decorator
 def throw_exception_file():
     raise IOError('Test error')
 
+@catch_io_error_with_wraps
+def read_does_not_exist_file():
+    with open("./training.notexist", 'r') as fd:
+        fd.read()
 
 def main():
-    throw_exception_file()
-
+    # throw_exception_file()
+    read_does_not_exist_file()
 
 if __name__ == "__main__":
     main()
