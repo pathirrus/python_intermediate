a58d2d8c1605b97dc2edea2e2d31a163eceb461f
Patryq <p.kurc@o2.pl>
2021-02-24 20:53:53 +0100

    Added code for exercises 5

--- lambda_ex/lambda_ex_4.py
+++ lambda_ex/lambda_ex_4.py
@@ -4,6 +4,11 @@ from random import randint
 def exercises_4():
     random_list = [randint(0, 101) for item in range(10)]        #lista składana
     print(f'Before: {random_list}')
-    print("After")
+    print(f"After and {type(random_list)}")
 
     print(list(map(lambda number: number*2, random_list)))
+def exercises_5():
+    rand_list = [randint(100, 200) for item in range(20)]
+    print(f'Random list:\n{rand_list}')
+    print(f'Sorted min to max:\n{sorted(rand_list)} ')
+    print(f'Sorted max to min:\n{sorted(rand_list, reverse=True)} ')
--- lambda_ex/main.py
+++ lambda_ex/main.py
@@ -1,9 +1,9 @@
-from python_intermediate_repo.python_intermediate.lambda_ex.lambda_ex_4 import exercises_4
+from python_intermediate_repo.python_intermediate.lambda_ex.lambda_ex_4 import exercises_4, exercises_5
 from python_intermediate_repo.python_intermediate.lambda_ex.lambda_exercises import exercises_1, exercises_2_3
 
 
 def main():
-    exercises_4()
+    exercises_5()
 
 
 if __name__ == '__main__':
